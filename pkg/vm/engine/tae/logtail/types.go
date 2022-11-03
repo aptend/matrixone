888bf0592743716a46d5b042a22c639a74ad9597
aptend <aptend@hotmail.com>
2022-11-03 12:03:27 +0800

    skip sys db and table in logtail

--- pkg/vm/engine/tae/logtail/types.go
+++ pkg/vm/engine/tae/logtail/types.go
@@ -184,6 +184,26 @@ var (
 	}
 )
 
+var (
+	skippedDB = map[uint64]bool{
+		pkgcatalog.MO_CATALOG_ID: true,
+	}
+
+	skippedTbl = map[uint64]bool{
+		pkgcatalog.MO_DATABASE_ID: true,
+		pkgcatalog.MO_TABLES_ID:   true,
+		pkgcatalog.MO_COLUMNS_ID:  true,
+	}
+)
+
+func shouldIgnoreDBInLogtail(id uint64) bool {
+	return skippedDB[id]
+}
+
+func shouldIgnoreTblInLogtail(id uint64) bool {
+	return skippedTbl[id]
+}
+
 func init() {
 	BlkMetaSchema = catalog.NewEmptySchema("blkMeta")
 

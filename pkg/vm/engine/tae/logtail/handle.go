af9f58efd8d4630dc314d03eecfbe4ecb589fa92
aptend <aptend@hotmail.com>
2026-06-15 23:24:02 +0800

    debug: restore logtail flush threshold to 90m

--- pkg/vm/engine/tae/logtail/handle.go
+++ pkg/vm/engine/tae/logtail/handle.go
@@ -86,7 +86,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const Size30M = 30 * 1024 * 1024
+const Size90M = 90 * 1024 * 1024
 
 type CheckpointClient interface {
 	CollectCheckpointsInRange(ctx context.Context, start, end types.TS) (ckpLoc string, lastEnd types.TS, err error)
@@ -189,7 +189,7 @@ func HandleSyncLogTailReq(
 
 	if canRetry { // check simple conditions first
 		_, name, forceFlush := fault.TriggerFault("logtail_max_size")
-		if (forceFlush && name == tableEntry.GetLastestSchemaLocked(false).Name) || resp.ProtoSize() > Size30M {
+		if (forceFlush && name == tableEntry.GetLastestSchemaLocked(false).Name) || resp.ProtoSize() > Size90M {
 			flushStart := time.Now()
 			flushErr := ckpClient.FlushTable(ctx, 0, did, tid, end)
 			// try again after flushing
